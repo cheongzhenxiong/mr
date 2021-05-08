427a1df4ecaa2b1ae9845d5f8299ba7af80a4bb7
peres <>
2021-05-08 17:05:24 +0900

    Handle some Jenkins REST errors.

--- mr/jenkins_client.py
+++ mr/jenkins_client.py
@@ -2,18 +2,49 @@
 # Jenkins API wrappers
 #
 
-from api4jenkins import Jenkins
+from .error import MrError
+from api4jenkins import Jenkins, ItemNotFoundError
 from .console import run_with_indeterminate_progress
 from time import sleep
 
 def start_task_wait_confirmation(username, token, server_url, job, parameters):
 	jenkins = Jenkins(server_url, auth=(username, token))
+	if not jenkins.exists():
+		raise CannotConnect(server_url)
 
-	qi = jenkins.build_job(job, **parameters)
+	try:
+		qi = jenkins.build_job(job, **parameters)
 
-	def go():
-		while not qi.get_build():
-			sleep(2)
-		return qi.get_build()
+		def go():
+			while not qi.get_build():
+				sleep(2)
+			return qi.get_build()
 
-	return run_with_indeterminate_progress(go)
+		return run_with_indeterminate_progress(go)
+
+	except ItemNotFoundError as e:
+		raise JobNotFound(job)
+
+	except Exception as e:
+		raise InvalidConfiguration(job, parameters)
+
+class CannotConnect(MrError):
+	def __init__(self, url):
+		self.url = url
+
+	def __str__(self):
+		return "Cannot connect to server at '{}'. Is it reachable?".format(self.url)
+
+class JobNotFound(MrError):
+	def __init__(self, job):
+		self.job = job
+
+	def __str__(self):
+		return "Cannot find job '{}'.".format(self.job)
+
+class InvalidConfiguration(MrError):
+	def __init__(self, job, params):
+		self.job = job
+
+	def __str__(self):
+		return "Invalid configuration for job '{}'.".format(self.job)
